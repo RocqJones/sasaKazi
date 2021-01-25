4e32d80586799f8b122c084cc2043ce9778176bc
RocqJones <jonesmbindyo@gmail.com>
2021-01-25 20:07:28 +0300

    logOut action

--- app/src/main/java/com/intoverflown/sasakazi/MainActivity.kt
+++ app/src/main/java/com/intoverflown/sasakazi/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.intoverflown.sasakazi
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.intoverflown.sasakazi.users.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Firebase
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nav_drawer)
+
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-//        val navController = findNavController(R.id.nav_host_fragment)
+        // val navController = findNavController(R.id.nav_host_fragment)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
@@ -27,5 +37,20 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    // logOut
+    fun logOutCurrentUser(item: MenuItem) {
+        mAuth = FirebaseAuth.getInstance()
+        mAuth!!.signOut()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
+    fun intentToProfile(item: MenuItem) {
+        TODO("Not yet implemented")
     }
 }
