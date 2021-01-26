12292f85c6fa9105ada6f49340745ec16f8013e7
RocqJones <jonesmbindyo@gmail.com>
2021-01-26 16:25:21 +0300

    User profile Layout and theme

--- app/src/main/java/com/intoverflown/sasakazi/MainActivity.kt
+++ app/src/main/java/com/intoverflown/sasakazi/MainActivity.kt
@@ -3,6 +3,8 @@ package com.intoverflown.sasakazi
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -15,12 +17,20 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.intoverflown.sasakazi.ui.navdrawer.ProfileActivity
 import com.intoverflown.sasakazi.users.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     // Firebase
     private var mAuth: FirebaseAuth? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mDatabaseReference: DatabaseReference? = null
+
+    //UI elements
+    private var fullName: TextView? = null
+    private var emailAddress: TextView? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -35,23 +45,23 @@ class MainActivity : AppCompatActivity() {
         // Bottom NavBar
         val navBottomView: BottomNavigationView = findViewById(R.id.nav_bottom_view)
 
-        // val navController = findNavController(R.id.nav_host_fragment)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
 
-//        val navController = findNavController(R.id.nav_host_fragment)
-
+        // val navController = findNavController(R.id.nav_host_fragment)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
         navBottomView.setupWithNavController(navController)
+
+        // initialize references
+        initializeReferences()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -59,9 +69,39 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
+    private fun initializeReferences() {
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        fullName = findViewById<View>(R.id.nav_fullName) as? TextView
+        emailAddress = findViewById<View>(R.id.nav_emailAddress) as? TextView
+    }
+
+    // show user full-name and email in the nav header
+    override fun onStart() {
+        super.onStart()
+
+        val mUser = mAuth!!.currentUser
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+
+//        emailAddress!!.text = mUser.email
+        emailAddress?.text ?: mUser.email
+
+        mUserReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+//                fullName!!.text = snapshot.child("fullname").value as String
+                fullName?.text ?: snapshot.child("fullname").value as String
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
     // logOut
     fun logOutCurrentUser(item: MenuItem) {
-        mAuth = FirebaseAuth.getInstance()
+        // mAuth = FirebaseAuth.getInstance()
         mAuth!!.signOut()
 
         val intent = Intent(this, LoginActivity::class.java)
@@ -70,7 +110,10 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+    // view User Profile
     fun intentToProfile(item: MenuItem) {
-        TODO("Not yet implemented")
+        val intent = Intent(this, ProfileActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/navdrawer/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.sasakazi.ui.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.sasakazi.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
