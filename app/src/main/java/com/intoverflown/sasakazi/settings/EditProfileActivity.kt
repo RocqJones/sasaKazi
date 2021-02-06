2a6c1ca5fb5fedc62591908218f62e236c0f68e9
RocqJones <jonesmbindyo@gmail.com>
2021-02-06 10:18:46 +0300

    edit profile activity skeleton

---
+++ app/src/main/java/com/intoverflown/sasakazi/settings/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.sasakazi.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.sasakazi.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
