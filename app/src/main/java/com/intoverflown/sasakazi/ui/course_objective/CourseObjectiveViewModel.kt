e9de86a9ed74c706568ce65021f41e5cced504b2
RocqJones <jonesmbindyo@gmail.com>
2021-02-18 18:56:05 +0300

    Course objective ViewModel

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveViewModel.kt
@@ -0,0 +1,7 @@
+package com.intoverflown.sasakazi.ui.course_objective
+
+import androidx.lifecycle.ViewModel
+
+class CourseObjectiveViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
