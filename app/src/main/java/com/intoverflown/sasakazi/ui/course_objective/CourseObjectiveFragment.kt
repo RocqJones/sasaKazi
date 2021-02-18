4609a91bd4a985a9da3800c4a043411cc77979b6
RocqJones <jonesmbindyo@gmail.com>
2021-02-18 18:55:48 +0300

    Course objective main kt

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveFragment.kt
@@ -0,0 +1,30 @@
+package com.intoverflown.sasakazi.ui.course_objective
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.intoverflown.sasakazi.R
+
+class CourseObjectiveFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CourseObjectiveFragment()
+    }
+
+    private lateinit var viewModel: CourseObjectiveViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.course_objective_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CourseObjectiveViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
