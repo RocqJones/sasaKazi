cb50597de34b43b1dfabb5d57943c7bff1cb88c9
RocqJones <jonesmbindyo@gmail.com>
2021-02-19 17:49:11 +0300

    course objective layout

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveFragment.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveFragment.kt
@@ -18,7 +18,7 @@ class CourseObjectiveFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.course_objective_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_course_objective, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
