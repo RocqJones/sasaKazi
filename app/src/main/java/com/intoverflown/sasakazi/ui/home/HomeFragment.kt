ffa456a98e4ed76fd86dc8fb02a61d277a64f69e
RocqJones <jonesmbindyo@gmail.com>
2021-02-19 18:38:21 +0300

    add intent to home screen TODO

--- app/src/main/java/com/intoverflown/sasakazi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/home/HomeFragment.kt
@@ -1,18 +1,26 @@
 package com.intoverflown.sasakazi.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.intoverflown.sasakazi.R
+import com.intoverflown.sasakazi.ui.course_objective.CourseObjectiveFragment
 
 class HomeFragment : Fragment() {
 
+
+
     private lateinit var homeViewModel: HomeViewModel
+
+    // UI
+    private var mobile_card: CardView? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -26,6 +34,19 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        // UI references
+        uiRefs()
+
         return root
+    }
+
+    private fun uiRefs() {
+        mobile_card!!.findViewById<View>(R.id.home_mobileAppCard)
+        mobile_card!!.setOnClickListener { intentToObjScreen() }
+    }
+
+    private fun intentToObjScreen() {
+        // TODO : add intent here
     }
 }
