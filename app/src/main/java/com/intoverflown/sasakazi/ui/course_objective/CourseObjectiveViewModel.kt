1e00d7a92ad0cc013d80cfd0b7fa619ab9402fa4
RocqJones <jonesmbindyo@gmail.com>
2021-02-19 17:47:35 +0300

    ViewModel updated

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveViewModel.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/CourseObjectiveViewModel.kt
@@ -1,7 +1,14 @@
 package com.intoverflown.sasakazi.ui.course_objective
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CourseObjectiveViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "You will learn the the basis of the Android platform and the application lifecycle. " +
+                "You will be able to write simple GUI applications, use built-in widgets and components, " +
+                "work with the database to store data locally, and much more by the end of this Android training course."
+    }
+    val text: LiveData<String> = _text
 }
