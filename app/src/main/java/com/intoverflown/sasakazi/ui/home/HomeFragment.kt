a9c3838e44d5962df873b777ed68ab25e42293b1
RocqJones <jonesmbindyo@gmail.com>
2021-02-22 16:37:22 +0300

    imports optimized

--- app/src/main/java/com/intoverflown/sasakazi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.intoverflown.sasakazi.ui.home
 
 import android.content.Intent
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
