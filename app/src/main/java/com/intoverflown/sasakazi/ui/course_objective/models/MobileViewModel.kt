40679683c00180eb6b99d9a08e2b46d7a5184770
RocqJones <jonesmbindyo@gmail.com>
2021-02-23 17:48:42 +0300

    Removed underscores

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/models/MobileViewModel.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/models/MobileViewModel.kt
@@ -8,28 +8,28 @@ class MobileViewModel : ViewModel() {
     private val _course = MutableLiveData<String>().apply {
         value = "Mobile App Development"
     }
-    private val _youtube_link = MutableLiveData<String>().apply {
+    private val _youtubeLink = MutableLiveData<String>().apply {
         value = "https://youtu.be/WvwwL0TwH6U"
     }
 
-    private val _course_objective = MutableLiveData<String>().apply {
+    private val _courseObjective = MutableLiveData<String>().apply {
         value = "You will learn the the basis of the Android platform and the application lifecycle. " +
                 "You will be able to write simple GUI applications, use built-in widgets and components, " +
                 "work with the database to store data locally, and much more by the end of this Android training course."
     }
 
-    private val _instructor_name = MutableLiveData<String>().apply {
+    private val _instructorName = MutableLiveData<String>().apply {
         value = "Jones Mbindyo"
     }
 
-    private val _cert_requirements = MutableLiveData<String>().apply {
+    private val _certRequirements = MutableLiveData<String>().apply {
         value = "Note: You must complete the Andela survey in your Pluralsight Skills, " +
                 "track-focused channel, in order to be eligible for Learning Phase I "
     }
 
     val course : LiveData<String> = _course
-    val youtube_link : LiveData<String> = _youtube_link
-    val text_course_obj : LiveData<String> = _course_objective
-    val text_instructor_name : LiveData<String> = _instructor_name
-    val text_cert_requirements : LiveData<String> = _cert_requirements
+    val youtubeLink : LiveData<String> = _youtubeLink
+    val textCourseObj : LiveData<String> = _courseObjective
+    val textInstructorName : LiveData<String> = _instructorName
+    val textCertRequirements : LiveData<String> = _certRequirements
 }
