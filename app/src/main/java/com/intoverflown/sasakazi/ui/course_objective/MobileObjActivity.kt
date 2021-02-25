1a4c88cffe89b45add7efcc5cdbe8e0e1f838696
RocqJones <jonesmbindyo@gmail.com>
2021-02-25 19:42:57 +0300

    YouTube video loader worked

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileObjActivity.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileObjActivity.kt
@@ -1,33 +1,28 @@
 package com.intoverflown.sasakazi.ui.course_objective
 
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.MediaController
 import android.widget.TextView
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.intoverflown.sasakazi.R
 import com.intoverflown.sasakazi.ui.course_objective.models.MobileViewModel
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.views.YouTubePlayerSeekBarListener
+
 
 class MobileObjActivity : AppCompatActivity() {
 
     // UI elements
     private var course_txtView : TextView? = null
-    private var intro_video : VideoView? = null
     private var obj_description : TextView? = null
     private var instructor_name : TextView? = null
     private var cert_conditions : TextView? = null
     private var vidPlayerView : YouTubePlayerView? = null
 
-    private var fullUrl : String? = null
-
+    private lateinit var fullUrl : String
     private lateinit var mobileViewModel : MobileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,21 +30,22 @@ class MobileObjActivity : AppCompatActivity() {
         setContentView(R.layout.activity_obj_mobile)
         mobileViewModel = ViewModelProvider(this).get(MobileViewModel::class.java)
 
-        course_txtView = findViewById<View>(R.id.courseTxtView) as TextView
-        intro_video = findViewById<View>(R.id.courseIntroVideoView) as VideoView
+//        course_txtView = findViewById<View>(R.id.courseTxtView) as TextView
+//        intro_video = findViewById<View>(R.id.courseIntroVideoView) as YouTubePlayerSupportFragment
         obj_description = findViewById<View>(R.id.objectiveDescription) as TextView
         instructor_name = findViewById<View>(R.id.instructorName) as TextView
         cert_conditions = findViewById<View>(R.id.conditionsList) as TextView
-        vidPlayerView = findViewById<View>(R.id.youtubePlayerView) as YouTubePlayerView
+        vidPlayerView = findViewById(R.id.youtubePlayerView)
+//        youtubeVidFrame = supportFragmentManager.findFragmentById(R.id.youTubeFrameLayout) as YouTubePlayerSupportFragment
 
         // set mutable LiveData
         setLiveDataHere()
     }
 
     private fun setLiveDataHere() {
-        mobileViewModel.course.observe(this) {
-            course_txtView!!.text = it
-        }
+//        mobileViewModel.course.observe(this) {
+//            course_txtView!!.text = it
+//        }
 
         mobileViewModel.textCourseObj.observe(this) {
             obj_description!!.text = it
@@ -63,8 +59,11 @@ class MobileObjActivity : AppCompatActivity() {
         }
 
         // get url and extract the link id in the subsequent function
-        fullUrl = mobileViewModel.youtubeLink.observe(this) { it }.toString()
-        val extractedVidID : String? = fullUrl!!.substringAfterLast("youtu.be/")
+        fullUrl = mobileViewModel.youtubeLink.observe(this) {
+            it
+        }.toString()
+
+        Log.i("YouTubeURL: ", fullUrl)
 
         playVideo()
     }
@@ -72,7 +71,29 @@ class MobileObjActivity : AppCompatActivity() {
 
     private fun playVideo() {
         // extract link id first
-        val extractedVidID : String? = fullUrl!!.substringAfterLast("youtu.be/")
+        val extractedVidID : String? = fullUrl.substringAfterLast("youtu.be/")
+//        youtubeVidFrame.initialize(YOUTUBE_API_KEY, YouTubePlayer.OnIniti)
+
+        vidPlayerView!!.let { lifecycle.addObserver(it) }
+//        get.addObserver(vidPlayerView!!)
+//        getLifecycle().addObserver(vidPlayerView!!)
+
+        vidPlayerView!!.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = extractedVidID
+//                val videoId = "snqMchRhQnk"
+                if (videoId != null) {
+                    Log.i("YouTubeID: ", videoId)
+                }
+//                youTubePlayer.loadVideo(videoId, 0F)
+                videoId.let {
+                    if (it != null) {
+                        youTubePlayer.loadVideo(it, 0F)
+                    }
+                }
+            }
+        })
+
 //        vidPlayerView?.let { getLifecycle().addObserver(it) }
 
 
