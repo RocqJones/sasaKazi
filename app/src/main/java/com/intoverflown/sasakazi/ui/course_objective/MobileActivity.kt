51ebc002fd10a1ada2769b6e5d821278d0584d4e
RocqJones <jonesmbindyo@gmail.com>
2021-03-02 15:11:08 +0300

    Float button object's click listener

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileActivity.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileActivity.kt
@@ -26,6 +26,21 @@ class MobileActivity : AppCompatActivity() {
     private var mobileFloatBtnView : View? = null
     private var mobileBaseLayout : ConstraintLayout? = null
 
+    // Float btn elements
+    private var mobileFloatTxtRes : TextView? = null
+    private var mobileFloatTxtObj : TextView? = null
+    private var mobileFloatTxtDis : TextView? = null
+    private var mobileFloatTxtAss : TextView? = null
+    private var mobileFloatResourceBtn : FloatingActionButton? = null
+    private var mobileFloatObjectiveBtn : FloatingActionButton? = null
+    private var mobileFloatAssignmentBtn : FloatingActionButton? = null
+    private var mobileFloatDiscussionBtn : FloatingActionButton? = null
+
+    // layouts View 'Gone'
+    private var mobileResourceVisibility: View? = null
+    private var mobileObjectiveVisibility: View? = null
+    private var mobileAssignmentVisibility: View? = null
+
     private lateinit var fullUrl : String
     private lateinit var viewModelMobile : ViewModelMobile
 
@@ -41,6 +56,21 @@ class MobileActivity : AppCompatActivity() {
         mobileFloatActionBtn = findViewById<View>(R.id.floatingActionBtn) as FloatingActionButton
         mobileFloatBtnView = findViewById(R.id.floatingBtnView)
         mobileBaseLayout = findViewById(R.id.rootLayout)
+
+        // Float btn elements
+        mobileFloatTxtObj = findViewById<View>(R.id.textViewObj) as TextView
+        mobileFloatTxtRes = findViewById<View>(R.id.textViewResources) as TextView
+        mobileFloatTxtDis = findViewById<View>(R.id.textViewDiscussion) as TextView
+        mobileFloatTxtAss = findViewById<View>(R.id.textViewAssignment) as TextView
+        mobileFloatResourceBtn = findViewById<View>(R.id.resources) as FloatingActionButton
+        mobileFloatObjectiveBtn = findViewById<View>(R.id.objective) as FloatingActionButton
+        mobileFloatAssignmentBtn = findViewById<View>(R.id.assignment) as FloatingActionButton
+        mobileFloatDiscussionBtn = findViewById<View>(R.id.discussion) as FloatingActionButton
+
+        // Views - include
+        mobileResourceVisibility = findViewById(R.id.resourcesVisibility)
+        mobileObjectiveVisibility = findViewById(R.id.objVisibility)
+        mobileAssignmentVisibility = findViewById(R.id.assignmentVisibility)
 
         // set mutable LiveData
         setLiveDataHere()
@@ -99,6 +129,55 @@ class MobileActivity : AppCompatActivity() {
                         mobileFloatBtnView!!.visibility = View.GONE
                     }
                 }
+
+                // show resources
+                mobileFloatResourceBtn!!.setOnClickListener {
+                    mobileResourceVisibility!!.visibility = View.VISIBLE
+                    mobileAssignmentVisibility!!.visibility = View.GONE
+                    mobileObjectiveVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                mobileFloatTxtRes!!.setOnClickListener {
+                    mobileResourceVisibility!!.visibility = View.VISIBLE
+                    mobileAssignmentVisibility!!.visibility = View.GONE
+                    mobileObjectiveVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                // show assignments
+                mobileFloatAssignmentBtn!!.setOnClickListener {
+                    mobileAssignmentVisibility!!.visibility = View.VISIBLE
+                    mobileObjectiveVisibility!!.visibility = View.GONE
+                    mobileResourceVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                mobileFloatTxtAss!!.setOnClickListener {
+                    mobileAssignmentVisibility!!.visibility = View.VISIBLE
+                    mobileObjectiveVisibility!!.visibility = View.GONE
+                    mobileResourceVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                // show course objective
+                mobileFloatObjectiveBtn!!.setOnClickListener {
+                    mobileObjectiveVisibility!!.visibility = View.VISIBLE
+                    mobileAssignmentVisibility!!.visibility = View.GONE
+                    mobileResourceVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                mobileFloatTxtObj!!.setOnClickListener {
+                    mobileObjectiveVisibility!!.visibility = View.VISIBLE
+                    mobileAssignmentVisibility!!.visibility = View.GONE
+                    mobileResourceVisibility!!.visibility = View.GONE
+                    mobileFloatBtnView!!.visibility = View.GONE
+                }
+
+                // Intent to Discussion screen
+//                mobileFloatDiscussionBtn!!.setOnClickListener { }
+//                mobileFloatTxtDis!!.setOnClickListener {  }
             }
 
             else {
