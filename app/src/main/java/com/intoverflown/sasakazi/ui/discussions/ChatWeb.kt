6657a91b68b19169d8c111f00e1048fcfb9e41c6
RocqJones <jonesmbindyo@gmail.com>
2021-03-06 23:27:06 +0300

    chat web activity

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussions/ChatWeb.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.sasakazi.ui.discussions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.sasakazi.R
+
+class ChatWeb : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chat_web)
+    }
+}
