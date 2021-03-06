d4094b8d706aa995b5195bf34bab9c79cc431d37
RocqJones <jonesmbindyo@gmail.com>
2021-03-06 23:35:33 +0300

    Mobile chat activity refactor

--- app/src/main/java/com/intoverflown/sasakazi/ui/discussions/ChatMobile.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussions/ChatMobile.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.intoverflown.sasakazi.R
 
-class ChatMobileActivity : AppCompatActivity() {
+class ChatMobile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat_mobile)
+        setContentView(R.layout.chat_mobile)
     }
 }
