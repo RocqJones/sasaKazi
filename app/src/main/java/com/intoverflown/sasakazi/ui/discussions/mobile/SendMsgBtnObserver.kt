fc8ca816dec11b3d0ffaad4c015184410fe40eb8
RocqJones <jonesmbindyo@gmail.com>
2021-03-10 10:31:15 +0300

    Button changed to ImageButton

--- app/src/main/java/com/intoverflown/sasakazi/ui/discussions/mobile/SendMsgBtnObserver.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussions/mobile/SendMsgBtnObserver.kt
@@ -2,12 +2,12 @@ package com.intoverflown.sasakazi.ui.discussions.mobile
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
+import android.widget.ImageButton
 
-class SendMsgBtnObserver(private val mButton: Button) : TextWatcher {
+class SendMsgBtnObserver(private val mButton: ImageButton?) : TextWatcher {
     override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
     override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-        mButton.isEnabled = charSequence.toString().trim { it <= ' ' }.isNotEmpty()
+        mButton!!.isEnabled = charSequence.toString().trim { it <= ' ' }.isNotEmpty()
     }
 
     override fun afterTextChanged(editable: Editable) {}
