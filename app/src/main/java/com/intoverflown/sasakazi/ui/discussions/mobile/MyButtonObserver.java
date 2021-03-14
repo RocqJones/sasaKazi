4036d3b9a116a853b821da12ad93d34094a1b02f
RocqJones <jonesmbindyo@gmail.com>
2021-03-14 23:00:59 +0300

    Img button observer

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussions/mobile/MyButtonObserver.java
@@ -0,0 +1,27 @@
+package com.intoverflown.sasakazi.ui.discussions.mobile;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ImageButton;
+
+public class MyButtonObserver implements TextWatcher {
+
+    private final ImageButton mButton;
+
+    public MyButtonObserver(ImageButton button) {
+        this.mButton = button;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        mButton.setEnabled(charSequence.toString().trim().length() > 0);
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+    }
+
+}
