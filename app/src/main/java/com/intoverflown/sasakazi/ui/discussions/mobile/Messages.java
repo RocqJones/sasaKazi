54527ef414a014813fb16143762d30dc26084742
RocqJones <jonesmbindyo@gmail.com>
2021-03-15 11:16:34 +0300

    refactored name to fullname

--- app/src/main/java/com/intoverflown/sasakazi/ui/discussions/mobile/Messages.java
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussions/mobile/Messages.java
@@ -3,38 +3,38 @@ package com.intoverflown.sasakazi.ui.discussions.mobile;
 public class Messages {
 
     private String text;
-    private String name;
+    private String fullname;
     private String photoUrl;
     private String imageUrl;
 
     public Messages() {
     }
 
-    public Messages(String text, String name, String photoUrl, String imageUrl) {
+    public Messages(String text, String fullname, String photoUrl, String imageUrl) {
         this.text = text;
-        this.name = name;
+        this.fullname = fullname;
         this.photoUrl = photoUrl;
         this.imageUrl = imageUrl;
+    }
+
+    public String getText() {
+        return text;
     }
 
     public void setText(String text) {
         this.text = text;
     }
 
-    public String getname() {
-        return name;
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
     }
 
-    public void setname(String name) {
-        this.name = name;
+    public String getFullname() {
+        return fullname;
     }
 
     public String getPhotoUrl() {
         return photoUrl;
-    }
-
-    public String getText() {
-        return text;
     }
 
     public void setPhotoUrl(String photoUrl) {
