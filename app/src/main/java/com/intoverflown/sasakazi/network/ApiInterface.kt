c223caa90dca6b634e6fc9592ad804b0fae6d4b0
RocqJones <jonesmbindyo@gmail.com>
2021-03-22 15:20:09 +0300

    api interface

--- app/src/main/java/com/intoverflown/sasakazi/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/sasakazi/network/ApiInterface.kt
@@ -1,11 +1,11 @@
 package com.intoverflown.sasakazi.network
 
 import com.intoverflown.sasakazi.data.PostModel
-import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.Response
+import retrofit2.http.GET
 
 interface ApiInterface {
     // Mobile data
-    @GET("/api/courses/")
-    fun fetchMobileData(): Call<List<PostModel>>
+    @GET("api/courses/2")
+    suspend fun fetchMobileData(): Response<PostModel>
 }
