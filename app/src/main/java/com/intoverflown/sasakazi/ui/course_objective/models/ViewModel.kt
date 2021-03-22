a596cbcb7f337fd86fb7bc37f2cb6d89f9e19e1a
RocqJones <jonesmbindyo@gmail.com>
2021-03-22 15:39:09 +0300

    ViewModel class

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/models/ViewModel.kt
@@ -0,0 +1,22 @@
+package com.intoverflown.sasakazi.ui.course_objective.models
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.intoverflown.sasakazi.data.PostModel
+import com.intoverflown.sasakazi.data.Repository
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class ViewModel(private val repository: Repository) : ViewModel() {
+
+    private val myMobileResponse: MutableLiveData<Response<PostModel>> = MutableLiveData()
+
+    fun fetchMobileData() {
+        // use coroutines
+        viewModelScope.launch {
+            val response = repository.fetchMobileData()
+            myMobileResponse.value = response
+        }
+    }
+}
