0b6cca3be377825bc18fce54fa609fe85ad71542
RocqJones <jonesmbindyo@gmail.com>
2021-03-22 15:32:46 +0300

    Data: Repository

--- app/src/main/java/com/intoverflown/sasakazi/data/Repository.kt
+++ app/src/main/java/com/intoverflown/sasakazi/data/Repository.kt
@@ -1,4 +1,14 @@
 package com.intoverflown.sasakazi.data
 
+import com.intoverflown.sasakazi.network.RetrofitInstance
+import retrofit2.Response
+
+/**  fetch all the data **/
 class Repository {
+    // Fetch Mobile Data
+    suspend fun fetchMobileData() : Response<PostModel> {
+        return RetrofitInstance.api.fetchMobileData()
+    }
+
+    // Fetch Web Data
 }
