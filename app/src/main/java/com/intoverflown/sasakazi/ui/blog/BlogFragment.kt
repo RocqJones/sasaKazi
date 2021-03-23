dc20fcffdb7f5c6062ec63ff3dd4bb3b71ae0201
RocqJones <jonesmbindyo@gmail.com>
2021-03-23 17:54:47 +0300

    OnBackPressedCallback

--- app/src/main/java/com/intoverflown/sasakazi/ui/blog/BlogFragment.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/blog/BlogFragment.kt
@@ -1,6 +1,7 @@
 package com.intoverflown.sasakazi.ui.blog
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.intoverflown.sasakazi.R
+import com.intoverflown.sasakazi.ui.home.HomeFragment
 
 class BlogFragment : Fragment() {
 
@@ -41,8 +43,7 @@ class BlogFragment : Fragment() {
                 }
             }
         }
-
-        requireActivity().onBackPressedDispatcher.addCallback(callback)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
         return root
     }
