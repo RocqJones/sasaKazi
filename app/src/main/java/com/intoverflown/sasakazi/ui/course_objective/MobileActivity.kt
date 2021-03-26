1a5e3747441d3a5727f4c4fdbde3520dbf2be5b7
RocqJones <jonesmbindyo@gmail.com>
2021-03-26 18:19:22 +0300

    Mobile course objective

--- app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileActivity.kt
+++ app/src/main/java/com/intoverflown/sasakazi/ui/course_objective/MobileActivity.kt
@@ -8,9 +8,13 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.intoverflown.sasakazi.R
+import com.intoverflown.sasakazi.data.Repository
+import com.intoverflown.sasakazi.ui.course_objective.models.ViewModel
+import com.intoverflown.sasakazi.ui.course_objective.models.ViewModelFactory
 import com.intoverflown.sasakazi.ui.course_objective.models.ViewModelMobile
 import com.intoverflown.sasakazi.ui.discussions.ChatMobile
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
@@ -24,6 +28,16 @@ class MobileActivity : AppCompatActivity() {
     private var mobileObjDescription : TextView? = null
     private var mobileInstructorName : TextView? = null
     private var mobileCertConditions : TextView? = null
+
+    private var mobileResourceLinks : TextView? = null
+    private var mobileResourceDescription : TextView? = null
+    private var mobileResourceBooks : TextView? = null
+    private var mobileResourceTranscripts : TextView? = null
+
+    private var mobileAssignmentDescription : TextView? = null
+    private var mobileAssignmentExercises : TextView? = null
+    private var mobileAssignmentTasks : TextView? = null
+
     private var mobileVidPlayerView : YouTubePlayerView? = null
     private var mobileFloatActionBtn : FloatingActionButton? = null
     private var mobileFloatBtnView : View? = null
@@ -42,11 +56,12 @@ class MobileActivity : AppCompatActivity() {
 
     private lateinit var fullUrl : String
     private lateinit var viewModelMobile : ViewModelMobile
+    private lateinit var viewModel: ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_obj_mobile)
-        viewModelMobile = ViewModelProvider(this).get(ViewModelMobile::class.java)
+//        viewModelMobile = ViewModelProvider(this).get(ViewModelMobile::class.java)
 
         mobileObjDescription = findViewById<View>(R.id.objectiveDescription) as TextView
         mobileInstructorName = findViewById<View>(R.id.instructorName) as TextView
@@ -67,53 +82,122 @@ class MobileActivity : AppCompatActivity() {
         mobileObjectiveVisibility = findViewById(R.id.objVisibility)
         mobileAssignmentVisibility = findViewById(R.id.assignmentVisibility)
 
-        // set mutable LiveData
+        // Resources
+        mobileResourceLinks = findViewById<View>(R.id.resourceLinks) as TextView
+        mobileResourceDescription = findViewById<View>(R.id.resourceDescription) as TextView
+        mobileResourceBooks = findViewById<View>(R.id.resourceBooks) as TextView
+        mobileResourceTranscripts = findViewById<View>(R.id.resourceTranscripts) as TextView
+
+        // assignment
+        mobileAssignmentDescription = findViewById<View>(R.id.assignmentDescription) as TextView
+        mobileAssignmentExercises = findViewById<View>(R.id.assignmentExercises) as TextView
+        mobileAssignmentTasks = findViewById<View>(R.id.assignmentTasks) as TextView
+
+        // initialize ViewModels
+        val repository = Repository()
+        val viewModelFactory = ViewModelFactory(repository)
+
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ViewModel::class.java)
+
+
         setLiveDataHere()
     }
 
     private fun setLiveDataHere() {
-        viewModelMobile.textCourseObj.observe(this) {
-            mobileObjDescription!!.text = it
-        }
+        viewModel.fetchMobileData()
+        viewModel.myMobileResponse.observe(this, Observer { response ->
+            if (response.isSuccessful) {
+                Log.d("Response", response.body()?.id.toString())
+                Log.d("Response", response.body()?.course_id!!)
+                Log.d("Response", response.body()?.course_title!!)
+                Log.d("Response", response.body()?.course_video_url!!)
+                Log.d("Response", response.body()?.course_objective!!)
+                Log.d("Response", response.body()?.course_instructor!!)
+                Log.d("Response", response.body()?.course_certificate_eligibility!!)
+                Log.d("Response", response.body()?.course_resources!!)
+                Log.d("Response", response.body()?.created_at!!)
+                Log.d("Response", response.body()?.updated_at!!)
 
-        viewModelMobile.textInstructorName.observe(this) {
-            mobileInstructorName!!.text = it
-        }
-        viewModelMobile.textCertRequirements.observe(this) {
-            mobileCertConditions!!.text = it
-        }
+                // set to UI
+                mobileObjDescription!!.text = response.body()?.course_objective
+                mobileInstructorName!!.text = response.body()?.course_instructor
+                mobileCertConditions!!.text = response.body()?.course_certificate_eligibility
 
-        // get url and extract the link id in the subsequent function
-        viewModelMobile.youtubeLink.observe(this) { it ->
-            fullUrl = it
+                // get url and extract the link id as follows
+                fullUrl = response.body()?.course_video_url!!
+                Log.i("YouTubeURL: ", fullUrl)
 
-            Log.i("YouTubeURL: ", fullUrl)
+                // extract link id from url
+                val extractedVidID : String? = fullUrl.substringAfterLast("youtu.be/")
 
-            // extract link id from url
-            val extractedVidID : String? = fullUrl.substringAfterLast("youtu.be/")
+                mobileVidPlayerView!!.let { lifecycle.addObserver(it) }
 
-            mobileVidPlayerView!!.let { lifecycle.addObserver(it) }
+                mobileVidPlayerView!!.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                    override fun onReady(youTubePlayer: YouTubePlayer) {
+                        if (extractedVidID != null) {
+                            Log.i("YouTubeID: ", extractedVidID)
+                        }
 
-            mobileVidPlayerView!!.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-                override fun onReady(youTubePlayer: YouTubePlayer) {
-                    if (extractedVidID != null) {
-                        Log.i("YouTubeID: ", extractedVidID)
-                    }
+                        extractedVidID.let { it1 ->
+                            if (it1 != null) {
+                                youTubePlayer.cueVideo(it1, 0F)
 
-                    extractedVidID.let { it1 ->
-                        if (it1 != null) {
-                            youTubePlayer.cueVideo(it1, 0F)
-
+                            }
                         }
                     }
-                }
-            })
+                })
 
-//            mobileVidPlayerView!!.enterFullScreen()
-//            mobileVidPlayerView!!.exitFullScreen()
-//            mobileVidPlayerView!!.isFullScreen()
-//            mobileVidPlayerView!!.toggleFullScreen()
-        }
+                // resources ui
+                mobileResourceLinks!!.text = response.body()?.course_resources
+
+                // assignment ui
+            } else {
+                Log.e("Response API Error: ", response.errorBody().toString())
+            }
+        })
+
+//        viewModelMobile.textCourseObj.observe(this) {
+//            mobileObjDescription!!.text = it
+//        }
+//
+//        viewModelMobile.textInstructorName.observe(this) {
+//            mobileInstructorName!!.text = it
+//        }
+//        viewModelMobile.textCertRequirements.observe(this) {
+//            mobileCertConditions!!.text = it
+//        }
+
+        // get url and extract the link id in the subsequent function
+//        viewModelMobile.youtubeLink.observe(this) { it ->
+//            fullUrl = it
+//
+//            Log.i("YouTubeURL: ", fullUrl)
+//
+//            // extract link id from url
+//            val extractedVidID : String? = fullUrl.substringAfterLast("youtu.be/")
+//
+//            mobileVidPlayerView!!.let { lifecycle.addObserver(it) }
+//
+//            mobileVidPlayerView!!.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+//                override fun onReady(youTubePlayer: YouTubePlayer) {
+//                    if (extractedVidID != null) {
+//                        Log.i("YouTubeID: ", extractedVidID)
+//                    }
+//
+//                    extractedVidID.let { it1 ->
+//                        if (it1 != null) {
+//                            youTubePlayer.cueVideo(it1, 0F)
+//
+//                        }
+//                    }
+//                }
+//            })
+//
+////            mobileVidPlayerView!!.enterFullScreen()
+////            mobileVidPlayerView!!.exitFullScreen()
+////            mobileVidPlayerView!!.isFullScreen()
+////            mobileVidPlayerView!!.toggleFullScreen()
+//        }
 
         mobileFloatActionBtn!!.setOnClickListener {
             if (mobileFloatBtnView!!.visibility == View.GONE) {
