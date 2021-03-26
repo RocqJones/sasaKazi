3ab2f175a3fc43522d7906f900bcdb2fae59f1ff
RocqJones <jonesmbindyo@gmail.com>
2021-03-26 22:56:53 +0300

    Migrations Discussion: Messages Constructor

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/discussion_migrations/reusableutils/Messages.kt
@@ -0,0 +1,16 @@
+package com.intoverflown.sasakazi.ui.discussion_migrations.reusableutils
+
+class Messages {
+    var text: String? = null
+    var fullname: String? = null
+    var photoUrl: String? = null
+    var imageUrl: String? = null
+
+    constructor() {}
+    constructor(text: String?, fullname: String?, photoUrl: String?, imageUrl: String?) {
+        this.text = text
+        this.fullname = fullname
+        this.photoUrl = photoUrl
+        this.imageUrl = imageUrl
+    }
+}
