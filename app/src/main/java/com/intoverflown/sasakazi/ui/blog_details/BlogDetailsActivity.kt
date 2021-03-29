4dc9e687c112ae32d12ea4a2c893196da4d9e2a7
RocqJones <jonesmbindyo@gmail.com>
2021-03-29 19:17:32 +0300

    Blog Details: Empty activity

---
+++ app/src/main/java/com/intoverflown/sasakazi/ui/blog_details/BlogDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.sasakazi.ui.blog_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.sasakazi.R
+
+class BlogDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blog_details)
+    }
+}
